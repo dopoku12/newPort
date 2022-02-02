646d80cf84a5f7524fb061668ef1429e77d2e326
David Opoku <71300793+dopoku12@users.noreply.github.com>
2022-02-01 22:25:37 -0500

    new port

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+// let Body = document.querySelector("body");
+// const changeCL = true;
+// document.querySelector(".checkbox").eventListener("Click", function () {
+//   if (changeCL) {
+//     Body.style.backgroundColor = "black";
+//   }
+
+// let x = [1, 2, 3, 4, 5];
